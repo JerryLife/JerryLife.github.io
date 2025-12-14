02450697d2a27e76c7eac184ca71ccbde319dc19
Jerry_Life <HUSTWuzhaomin@outlook.com>
2025-12-15 01:48:47 +0800

    feat: Safelist dynamically added CSS classes for author highlights, filters, and selected papers. to ensure deployed site preserves colors

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,15 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  safelist: [
+    // Author highlight classes (dynamically added via Liquid)
+    "first-author",
+    "corresponding-author",
+    // Filter classes (added via JavaScript)
+    "filter-first-author",
+    "filter-corresponding-author",
+    "active",
+    // Selected papers section
+    "selected-papers",
+  ],
 };
